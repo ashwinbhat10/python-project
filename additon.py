a5d48251b065a07ae01ef26d1d3bd5c76a7eed7a
ashwin <ashwinbhat10@gmail.com>
2017-06-03 11:47:48 +0530

    added new test script

--- additon.py
+++ additon.py
@@ -1,10 +1,19 @@
 import os
+import unittest
 
 def addition (a,b) :
     return a+b
 
+class test_addition(unittest.TestCase) :
+
+    def test_add(self):
+        self.assertEqual(5,addition(2,3))
 
 
-print addition(3,4)
+
+if __name__ == '__main__' :
+   print "testing"
+   unittest.main()
+
 
 
